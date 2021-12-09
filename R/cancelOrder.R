8d9c48d26d1f326793503cc260dcc3fb1d6850d8
Aleksandr Kulikov <unnotigkeit@yandex.ru>
2021-12-09 21:43:05 +0300

    Delete cancelOrder.R

--- R/cancelOrder.R
+++
@@ -1,8 +0,0 @@
-cancelOrder <- function(Connection, OrderID) 
-{
-    url <- paste0("https://hft-api.lykke.com/api/Orders/",OrderID, "/Cancel")
-	resp <- httr::POST(url,
-                     httr::add_headers(`api-key` = conn$key),
-                     encode = "json")					
-    return(resp)
-}
