571df610e9f687c4e48267cf7d6b81e14479c6f7
Aleksandr Kulikov <unnotigkeit@yandex.ru>
2021-12-09 21:43:50 +0300

    Delete getOrders.R

--- R/getOrders.R
+++
@@ -1,9 +0,0 @@
-getOrders <- function(Connection) 
-{
-        url <- "https://hft-api.lykke.com/api/Orders"
-
-        resp <- httr::GET(url,
-                           httr::add_headers(`api-key` = conn$key),
-                           encode = "json")
-        return(content(resp, "parsed"))
-}
